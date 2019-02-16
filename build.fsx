6178c07822bbc1a33de56268d819116c64ae4c7c
humhei <humhei@outlook.com>
2019-02-16 16:11:47 +0800

    Test

--- build.fsx
+++ build.fsx
@@ -1,5 +1,13 @@
 #if FAKE
-
-#r "paket:
-
+#r "paket: groupref Main //"
 #endif
+#if !FAKE
+#r "Facades/netstandard"
+#r "netstandard"
+#endif
+#load "./.fake/build.fsx/intellisense.fsx"
+
+open System
+open ClassLibrary1
+
+let s = ClassLibrary1.Class1.Run
